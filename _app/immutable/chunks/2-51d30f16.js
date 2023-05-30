db2e93b00523718ecd4a493619c804d04f683780
relaxtakenotes <134127331+jeffthekillerz@users.noreply.github.com>
2023-05-30 14:39:20 +0500

    fuck

--- _app/immutable/chunks/2-51d30f16.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-582a8b2d.js";export{t as component};
---
+++ _app/immutable/chunks/2-f11562c9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-bf2769b6.js";export{t as component};
