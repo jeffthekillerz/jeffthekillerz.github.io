7165f0293d8bb75b2bd02dd89f29885dde81e4aa
relaxtakenotes <134127331+jeffthekillerz@users.noreply.github.com>
2023-05-31 03:50:23 +0500

    yess

---
+++ _app/immutable/chunks/2-d5e48b70.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b1026616.js";export{t as component};
--- _app/immutable/chunks/2-f11562c9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-bf2769b6.js";export{t as component};
