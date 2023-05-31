8166114f24840a8822e979b56354f9835586a187
relaxtakenotes <134127331+jeffthekillerz@users.noreply.github.com>
2023-05-31 14:21:52 +0500

    ddos fail

---
+++ _app/immutable/chunks/2-ef10c51b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-37987c84.js";export{t as component};
--- _app/immutable/chunks/2-f72c242e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a1b652db.js";export{t as component};
